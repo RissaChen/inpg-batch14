0cc664287fab78633ac0b60139d33eb05761fa7e
RissaChen <c.amadea.r@gmail.com>
2019-06-11 16:59:52 +0700

    First commit

---
+++ day5/tes.js
@@ -0,0 +1,76 @@
+var score = 100;
+
+  console.log(score => 100 ~ 85); //Grade A
+  console.log(score => 84 ~ 75); //Grade B
+  console.log(score => 74 ~ 65); //Grade C
+  console.log(score => 64 ~ 55); //Grade D
+  console.log(score => 54 ~ 0); //Grade E
+
+`````````````````````````````````````````
+var score = 100
+
+  if(score === 100 ~ 85){ 
+    console.log("Grade A");
+  }
+
+  else if(score === 84 ~ 75) {
+    console.log("Grade B");
+  }
+  
+  else if(score 74 ~ 65) {
+    console.log("Grade C")
+}
+
+  else if(score 64 ~ 55) {
+    console.log("Grade D")
+}
+
+  else(score 54 ~ 0) {
+    console.log("Grade E")
+} 
+`````````````````````````````````````````````````
+var kecepatan = 20 km/jam
+var penumpang = 4 orang
+
+  if(penumpang lebih dari 2 orang){ 
+    console.log("kecepatan becak berkurang 5 km/jam");
+  }
+
+  else if(4 penumpang) {
+    console.log("kecepatang berkurang 7 km/jam");
+  }
+  
+  else if(1 atau 2 penumpang) {
+    console.log("kecepatan becak berkurang 2 km/jam")
+}
+
+`````````````````````````````````````````````````
+var nama = andre
+var password = 123
+var isLogin = false
+var role1 = admin
+var role2= super admin 
+var role3= selain admin dan super admin
+
+
+  if(true){
+  console.log("user login succes")
+}
+
+  else if(false){
+  console.log(user login failed)
+}
+
+  else if(true){
+  console.log(Role?)
+}
+
+  else if (true;role1;role2){
+  console.log("You have all access as role")
+}
+
+  else if(true;role3){
+  console.log("You have restricted access as role")
+}
+
+
